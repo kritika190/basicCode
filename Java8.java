1f693fd9cc2c8306048b2f4de74686eb5190023a
kritika190 <67952841+kritika190@users.noreply.github.com>
2022-11-14 11:02:17 +0530

    Update Java8.java

--- Java8.java
+++ Java8.java
@@ -1,4 +1,7 @@
 java 8 even odd
 
 list.stream().filter(x->x%2==0).forEach(x->System.out.print(x))
-  list.stream().filter(x->x%2!=0).forEach(x->System.out.print(x))
+list.stream().filter(x->x%2!=0).forEach(x->System.out.print(x))
+  
+  
+  thankyou:)
